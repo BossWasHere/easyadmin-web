c98b1b5c0b3e3d285280dd6d60e67cd065c0db2c
BossWasHere <22352723+BossWasHere@users.noreply.github.com>
2022-07-20 23:18:51 +0100

    feat: add nav pages

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,7 +6,12 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '/', component: () => import('pages/IndexPage.vue') },
+      { path: '/admin', component: () => import('pages/AdminSettingsPage.vue') },
       { path: '/dashboard', component: () => import('pages/DashboardPage.vue') },
+      { path: '/logs', component: () => import('pages/LogsPage.vue') },
+      { path: '/me', component: () => import('pages/UserSettingsPage.vue') },
+      { path: '/moderation', component: () => import('pages/ModerationAnalyticsPage.vue') },
+      { path: '/network', component: () => import('pages/NetworkAnalyticsPage.vue') },
     ],
   },
 
